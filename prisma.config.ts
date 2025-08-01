e5d37d27f23a80365488ba0727d6aa198a360470
Astesola <Astesola@gmail.com>
2025-08-01 20:29:16 +0800

    Trans to D1

---
+++ prisma.config.ts
@@ -0,0 +1,20 @@
+import path from "node:path";
+import type { PrismaConfig } from "prisma";
+import { PrismaD1 } from "@prisma/adapter-d1";
+
+// import your .env file
+import "dotenv/config";
+
+export default {
+  experimental: {
+    adapter: true
+  },
+  schema: path.join("prisma", "schema.prisma"),
+  async adapter() {
+    return new PrismaD1({
+      CLOUDFLARE_D1_TOKEN: process.env.CLOUDFLARE_D1_TOKEN!,
+      CLOUDFLARE_ACCOUNT_ID: process.env.CLOUDFLARE_ACCOUNT_ID!,
+      CLOUDFLARE_DATABASE_ID: process.env.CLOUDFLARE_DATABASE_ID!,
+    });
+  },
+} satisfies PrismaConfig;
