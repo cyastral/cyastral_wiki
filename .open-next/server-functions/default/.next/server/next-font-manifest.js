b581913d3740d4749743a9d3795b32c1747b8b1f
astesola <s2899138166@gmail.com>
2025-08-15 07:16:21 +0800

    delete cached

--- .open-next/server-functions/default/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/astesola/Project/cyastral_wiki/src/app/layout":["static/media/569ce4b8f30dc480-s.p.woff2","static/media/93f479601ee12b01-s.p.woff2","static/media/47cbc4e2adbc5db9-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
